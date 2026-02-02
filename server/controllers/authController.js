621981e4b3a1d714de4fd52509c281fb6363f110
FluffySce <73637814+FluffySce@users.noreply.github.com.>
2026-02-02 11:16:14 +0530

    feat: add authentication logic

---
+++ server/controllers/authController.js
@@ -0,0 +1,37 @@
+import User from "../models/userModel";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  const { username, password, role } = req.body;
+  try {
+    const user = new User({
+      username,
+      password,
+      role,
+    });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ message: "Error registering user", error: err.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username });
+    if (!user || !(await user.checkPassword(password))) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Login error", error: err.message });
+  }
+};
